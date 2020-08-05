6a88468a5d297df501ae1d123b50ac49e49f3ddc
chao.li <chao.li@team-lab.cn>
2020-08-05 17:33:41 +0800

    Initial Commit

---
+++ OpenGL_ES_ParticleSystem/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OpenGL_ES_ParticleSystem
+//
+//  Created by tlab on 2020/8/5.
+//  Copyright © 2020 yuanfangzhuye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
